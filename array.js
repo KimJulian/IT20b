4ce0665a752877b5d596caab9d9c00296840f16f
Kimjulian <mentopakimjulian@gmail.com>
2025-11-25 08:19:21 +0800

    Added Update method

--- array.js
+++ array.js
@@ -57,6 +57,17 @@ class Array{
         this.length--;
 
     }
+
+    update(index,element){
+        //Correcting Statement || Must be False
+        if(index <0 || index > this.length){
+            console.log("Invalid Index");
+            return;
+        }
+
+        //After Condition
+        this.data[index] = element;
+    }
 }
 
 //
@@ -65,5 +76,9 @@ arr1 = new Array([4,2,3]);
 arr1.traverse();
 arr1.insert(0,9);
 arr1.traverse();
+
 arr1.delete(1);
+arr1.traverse();
+
+arr1.update(2,5);
 arr1.traverse();

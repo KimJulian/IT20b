23777a456f7f2d899bd999dd6a1d202fcdcef654
Kimjulian <mentopakimjulian@gmail.com>
2025-11-25 08:42:57 +0800

    Added is_empty Method Utility

--- stack.js
+++ stack.js
@@ -19,12 +19,24 @@ class Stack{
 
         console.log(output);
     }
+
+    is_empty(){
+        return this.topIndex === -1;
+    }
+
+    
 }
 
 //
 stack1 = new Stack();
 
+console.log(stack.is_empty());
 stack1.push(4);
 stack1.push(2);
 stack1.push(3);
+stack1.push(5);
+stack1.push(8);
 stack1.traverse();
+console.log(stack.is_empty());
+console.log(stack1.size());
+console.log("The top element is " + stack1.peek());

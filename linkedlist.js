bf661d6f79c0c82a44f428a28a4355abee578bb9
kimjulian <mentopakimjulian@gmail.com>
2025-12-02 09:08:27 +0800

    Added search utility method in class LinkedList

--- linkedlist.js
+++ linkedlist.js
@@ -61,6 +61,16 @@ class LinkedList{
         console.log("Target not Found");
     }
 
+    search(element){
+        let current = this.head;
+
+        while(current){
+            if(current.value === element)
+            return current;
+            current = current.next;
+        }
+        return "Element not found"
+    }
 }
 
 let list1 = new LinkedList();
@@ -68,5 +78,6 @@ list1.insert_at_end(4);
 list1.insert_at_beginning(2);
 list1.insert_at_end(3);
 console.log(list1.traverse());
-list1.insert_after(5,17);
+list1.insert_after(4,17);
 console.log(list1.traverse());
+console.log(list1.search(4));
